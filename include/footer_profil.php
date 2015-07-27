fc2ad737708fb9cf6d110f3c82fd784a9b8f5128
Gwen-Pigat <pixofheaven@gmail.com>
2015-07-27 13:38:45 +0200

    MAJ des fonts pour responsive (px/em vers vw)

--- include/footer_profil.php
+++ include/footer_profil.php
@@ -1,26 +1,26 @@
 <section class="contact_us text-center col-md-12">
- <h1>Des questions ? <a href="../contact.php">Contactez-nous</a></h1>
+ <h3>Des questions ? <a href="../contact.php">Contactez-nous</a></h3>
 
  <div class="social">
         <ul class="social-list">
             <li>
                 <a href="https://www.linkedin.com/company/5304501?trk=prof-0-ovw-curr_pos" target="_blank">
-                    <i class="fa fa-linkedin-square fa-5x"></i>
+                    <i class="fa fa-linkedin-square fa-3x"></i>
                 </a>
             </li>
             <li>
                 <a href="https://facebook.com/thecampusjob" target="_blank">
-                    <i class="fa fa-facebook-square fa-5x"></i>
+                    <i class="fa fa-facebook-square fa-3x"></i>
                 </a>
             </li>
             <li>
                 <a href="https://twitter.com/thecampusjob" target="_blank">
-                    <i class="fa  fa-twitter-square fa-5x"></i>
+                    <i class="fa  fa-twitter-square fa-3x"></i>
                 </a>
             </li>
             <li>
                 <a target="_blank" href="https://www.youtube.com/user/thecampusjob">
-                    <i class="fa fa-youtube-square fa-5x"></i>
+                    <i class="fa fa-youtube-square fa-3x"></i>
                 </a>
             </li>
         </ul>
