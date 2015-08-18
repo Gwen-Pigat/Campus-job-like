1af78ac5595def40ae188ab5144fd0a76c920e81
Gwen-Pigat <pixofheaven@gmail.com>
2015-08-18 16:09:20 +0200

    Mise à jour dee profilWait() pour supprimer les profils en attente

--- include/functions.php
+++ include/functions.php
@@ -10,6 +10,8 @@ class sessionHandle{
 	function profilWait(){
 		$link = new mysqli("localhost","root","motdepasselocalhostgwen","JobFinder")or die("Erreur Connexion BDD");
 		$link->query("DELETE FROM EntrepriseProfil WHERE Statut_profil='En attente'");
+		$link->query("DELETE FROM Etudiant WHERE Statut_profil='En attente'");
+		$link->query("DELETE FROM Offre WHERE Statut='En attente'");
 	}
 	
 }
