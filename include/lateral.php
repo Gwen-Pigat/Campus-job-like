93f067fb5ccd7cb88c5d869ce2a87d3703698857
Gwen-Pigat <pixofheaven@gmail.com>
2015-08-18 15:52:04 +0200

    Liste de mes offres non dispo si row->offres est égal à 0

--- include/lateral.php
+++ include/lateral.php
@@ -6,7 +6,7 @@ $row = $query->fetch_object();
 if ($row->Statut_profil != "En attente") { ?>
 <nav class="lateral">
 	<div class="logo text-center">
-		<?php $query = $link->query("SELECT * FROM EntrepriseProfil WHERE id_crypt='$_SESSION->id'");
+		<?php $query = $link->query("SELECT * FROM EntrepriseProfil WHERE id_crypt='$_SESSION[id]'");
 		$row = $query->fetch_object(); ?>
  <?php 
 
@@ -20,9 +20,11 @@ else{
  <br><br>
   </div>
 	<ul class="nav nav-stacked span-hide">
-		<?php echo "<a href='job_submit.php'>" ?><li><i class="fa fa-user fa-3x"></i> <span class="lateral-aside">Mon profil</span></li></a>
-		<?php echo "<a href='offre_submit.php?poste_offre=$row->Entreprise'>" ?><li><i class="fa fa-plus-circle fa-3x"></i> <span class="lateral-aside">Poster une offre</span></li></a>
-		<?php echo "<a href='offre_submit.php?liste_offres=$row->Entreprise'>" ?><li><i class="fa fa-pencil-square-o fa-3x"></i> <span class="lateral-aside">Liste de mes offres</span></li></a>
+		<?php echo "<a href='index.php?Profil_employeur'>" ?><li><i class="fa fa-user fa-3x"></i> <span class="lateral-aside">Mon profil</span></li></a>
+		<?php echo "<a href='index.php?Profil_employeur&poster_offre'>" ?><li><i class="fa fa-plus-circle fa-3x"></i> <span class="lateral-aside">Poster une offre</span></li></a>
+		<?php if ($row->Offres > 0) {
+			  echo "<a href='index.php?Profil_employeur&liste_offres'>" ?><li><i class="fa fa-pencil-square-o fa-3x"></i> <span class="lateral-aside">Liste de mes offres</span></li></a>
+		<?php } ?>
 		<a href="include/logout.php"><li class="logout"><i class="fa fa-hand-o-left fa-3x"></i> <span class="lateral-aside">Déconnexion</span></li></a>
 	</ul>
 </nav>
