d6b46f02060474c359dd104743d3b5482ff839c4
Gwen-Pigat <pixofheaven@gmail.com>
2015-08-20 10:38:10 +0200

    Ajout du token de mot de passe et envoi de mail

--- php/redirect_master.php
+++ php/redirect_master.php
@@ -88,11 +88,52 @@ elseif (isset($_GET["inscription"]) && !isset($_GET['password_request']) && isse
 
 // Demande d'envoi de mot de passse
 
+elseif (isset($_GET['password_request']) && !isset($_GET['inscription']) && isset($_POST['email_send_etudiant']) && !empty($_POST['email_send_etudiant'])) {
+	echo "$_POST[email_send_etudiant]";
+	$query = $link->query("SELECT * FROM Etudiant WHERE Email='$_POST[email_send_etudiant]'")or die("Erreur query");
+	$row = $query->fetch_object();
 
+	if ($row == 1) {
 
+	$random = sha1(str_shuffle(0123456789)).sha1(str_shuffle("azertyuiopmlkjhgfdsq"));
+	$token = "TOK_//$random//$random";
 
+	$link->query("UPDATE Etudiant SET token_password='$token'");
+
+	// Envoi du mail
+	require "../PHPMailer/class.phpmailer.php";
+
+	//Envoi des données par mail
+
+    // Envoi à l'admin
+    $mail = new phpmailer();
+
+    // Define who the message is from
+    $mail->From = str_shuffle(0123456789);
+    $mail->FromName = 'JobFinder | Mot de passe';
+
+    // Set the subject of the message
+    $mail->Subject = "$row->Nom - $row->Prenom";
+
+    // Add the body of the message
+    $body = "Chèr $row->Prenom, voici le lien afin de réinitiliser votre mot de passe :\n\n\n
+    <div style='Margin-top: 10%; Border: 1px #000 solid; Padding: 2%'>
+    	<a href='http://bestialsoul.com/TestCampus/index.php?Etudiant&token=$row->token_password'>Cliquez ici</a>
+    </div>";
+
+    $mail->Body = $body;
+
+    // Add a recipient address
+    $mail->AddAddress("$row->Email");
+
+    if(!$mail->Send())
+        echo ('');
+    else
+        echo ('');
+
+	}
+}
 else{
-	// echo "FALSE";
 	header("Location: ../include/logout.php");
 }
 
