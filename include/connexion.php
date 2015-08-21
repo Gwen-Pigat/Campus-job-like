5a35f3b4c7b6a344c6bc0c7cb2d0d1a7c3053429
Gwen-Pigat <pixofheaven@gmail.com>
2015-08-21 09:51:07 +0200

    Condition si echec de la connexion

--- include/connexion.php
+++ include/connexion.php
@@ -1,4 +1,9 @@
 <?php 
 session_start();
 $link = new mysqli("localhost","root","motdepasselocalhostgwen","JobFinder");
+
+if ($link->connect_errno) {
+	print_f("Echec de la connexion: %s\n", $link->connect_error);
+}
+
 ?>
